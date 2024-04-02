1c76a8061bd70e4370641c0a15e802ade2aacca8
aek676 <aek676@inlumine.ual.es>
2024-04-02 14:16:27 +0200

    actualizado PremiosCineAppTest

---
+++ hmis2024Oscars/src/test/java/org/ualhmis/oscars/PremiosCineAppTest.java
@@ -0,0 +1,28 @@
+package org.ualhmis.oscars;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.Test;
+
+public class PremiosCineAppTest  {
+	@Test
+	void testMainMethod() {
+		PrintStream originalOut = System.out;
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(bos));
+
+		// action
+		PremiosCineApp p = new PremiosCineApp(); // Para llegar al 100%: creacion de objeto y llamada al constructor por defecto 
+		PremiosCineApp.main(null);
+
+		// assertion
+		String salida = "La película ganadora del Oscar a la Mejor Película es: Inception";
+		assertEquals(salida + System.lineSeparator(), bos.toString(), "La salida por consola no es exactamente igual a la esperada.");
+
+		// undo the binding in System
+		System.setOut(originalOut);
+	}
+}
+
