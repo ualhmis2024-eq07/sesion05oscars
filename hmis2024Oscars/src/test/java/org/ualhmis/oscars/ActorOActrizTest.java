2cbb222512df85be1545c665df729bcea4e7431a
Anass El Jabiry <aek676@inlumine.ual.es>
2024-04-30 13:14:09 +0200

    Hasta el paso 6 done, para actividad07

--- hmis2024Oscars/src/test/java/org/ualhmis/oscars/ActorOActrizTest.java
+++ hmis2024Oscars/src/test/java/org/ualhmis/oscars/ActorOActrizTest.java
@@ -1,31 +1,8 @@
 package org.ualhmis.oscars;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ActorOActrizTest {
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
 	@Test
 	void testActorOActriz() {
 		new ActorOActriz("Kate Winslet", "Femenino", "5/10/1975");
